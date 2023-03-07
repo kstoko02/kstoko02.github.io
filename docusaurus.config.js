aa66ef85c0c3f711c9a1256200efdf6c5e46e529
winter chen <katyamy1008@gmail.com>
2023-03-07 10:22:23 +0800

    ll

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,11 +11,11 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://kstoko02.github.io/docs',
+  url: 'https://kstoko02.github.io/build',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   trailingSlash: false ,
-  baseUrl: '/docs/',
+  baseUrl: '/build/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
